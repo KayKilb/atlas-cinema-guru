6a33066e1c10608790bacbf1ba2d35ad18d6b607
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-11-05 22:40:53 -0600

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,15 @@
-export { auth as middleware } from "@/auth";
+import { withAuth } from "next-auth/middleware";
 
+// Use withAuth to handle redirection to the login page for unauthenticated users
+export default withAuth({
+  pages: {
+    signIn: "/login", // Redirects to `/login` if user is not authenticated
+  },
+});
+
+// Middleware configuration to avoid applying to certain paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|logo.png).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|logo.png).*)", // Exclude public assets and API routes
+  ],
 };
