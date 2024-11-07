b8b8c1714bf2abe138d67e9ba6868761b35cb3f7
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-11-07 11:56:26 -0600

    auth issues

--- auth.ts
+++ auth.ts
@@ -1,17 +1,46 @@
 //auth.ts
+import { fetchUser } from "./lib/data";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
-    brandColor: "#1ED2AF",
+    brandColor: "#ffffff",
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
   providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID as string,
-      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      //@ts-ignore
+      authorize: async (credentials: { email: string; password: string }) => {
+        const { email, password } = credentials;
+        const user = await fetchUser(email);
+        if (!user) return null; //@ts-ignore
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) return user;
+        return null;
+      },
     }),
   ],
   callbacks: {
@@ -21,5 +50,3 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
-
-// export default NextAuth;
