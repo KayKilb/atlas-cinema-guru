1f4cc2ad7b1ba6fe9b3049cad467095fac2fe273
KayKilb <115676011+KayKilb@users.noreply.github.com>
2025-03-13 10:00:36 -0500

    auth to show github login page

--- auth.ts
+++ auth.ts
@@ -1,14 +1,48 @@
-// auth.ts
+//auth.ts
+import { fetchUser } from "./lib/data";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
-    brandColor: "#1ED2AF",
+    brandColor: "#ffffff",
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [GitHub],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      //@ts-ignore
+      authorize: async (credentials: { email: string; password: string }) => {
+        const { email, password } = credentials;
+        const user = await fetchUser(email);
+        if (!user) return null;
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) return user;
+        return null;
+      },
+    }),
+  ],
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
